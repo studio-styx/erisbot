a58980314c0ec3ee94d314f2d815281d71167242
BirdTool <guilhermealvesga0789@hotmail.com>
2025-10-31 11:12:58 -0300

    Imagem de jogo

--- src/discord/events/onMessage.ts
+++ src/discord/events/onMessage.ts
@@ -4,9 +4,12 @@ import { onMention } from "./onMention.js";
 import { xpSystem } from "./chat/xpSystem.js";
 import { onAfkMentioned } from "./onAfkMentioned.js";
 import { onResponseTryviaGame } from "./tryvia/response.js";
-import { Gender, PersonalityTrait } from "#prisma";
+import { FootballLeague, FootballMatch, FootballTeam, Gender, PersonalityTrait } from "#prisma";
 import { icon, registerFootballGames, res } from "#functions";
-import { brBuilder } from "@magicyan/discord";
+import { brBuilder, randomNumber } from "@magicyan/discord";
+import { MatchStatistics } from "#types/footballData/match.js";
+import { createMatchImage } from "functions/footballApi/images/match.js";
+import { AttachmentBuilder } from "discord.js";
 
 createEvent({
     name: "onMessage",
@@ -170,6 +173,111 @@ createEvent({
                         console.error(error);
                         await msg.edit(res.danger(`${icon.error} | Erro ao registrar partidas!`));
                     }
+                    break;
+                }
+                case "s.image": {
+                    type MatchType = (FootballMatch & {
+                        homeTeam: FootballTeam & { statistics?: MatchStatistics },
+                        awayTeam: FootballTeam & { statistics?: MatchStatistics },
+                        competition: FootballLeague,
+                        elapsed: string
+                    });
+
+                    function randomMatch(): MatchType {
+                        return {
+                            id: BigInt(123),
+                            apiId: 987654,
+                            venue: "Arena do Grêmio",
+                            startAt: new Date(),
+                            status: "FINISHED",
+                            homeTeamId: BigInt(1),
+                            awayTeamId: BigInt(2),
+                            competitionId: BigInt(1),
+                            goalsHome: 1,
+                            goalsAway: 2,
+                            oddsHomeWin: 2.3,
+                            oddsDraw: 3.7,
+                            oddsAwayWin: 2.5,
+                            homeTeam: {
+                                id: BigInt(1),
+                                apiId: BigInt(176),
+                                name: "Grêmio",
+                                shortName: "Grêmio",
+                                tla: "GRE",
+                                crest: "https://crests.football-data.org/176.png",
+                                address: "Porto Alegre",
+                                clubColors: "Blue, Black & White",
+                                venue: "Arena do Grêmio",
+                                areaId: 2032,
+                                points: 45,
+                                statistics: {
+                                    corner_kicks: 7,
+                                    free_kicks: 12,
+                                    goal_kicks: 5,
+                                    offsides: 3,
+                                    fouls: 8,
+                                    ball_possession: 53,
+                                    saves: 3,
+                                    throw_ins: 22,
+                                    shots: 12,
+                                    shots_on_goal: 7,
+                                    shots_off_goal: 5,
+                                    yellow_cards: 2,
+                                    yellow_red_cards: 0,
+                                    red_cards: 0,
+                                },
+                            },
+                            awayTeam: {
+                                id: BigInt(2),
+                                apiId: BigInt(177),
+                                name: "Internacional",
+                                shortName: "Inter",
+                                tla: "INT",
+                                crest: "https://crests.football-data.org/177.png",
+                                address: "Porto Alegre",
+                                clubColors: "Red & White",
+                                venue: "Beira-Rio",
+                                areaId: 2032,
+                                points: 48,
+                                statistics: {
+                                    corner_kicks: 4,
+                                    free_kicks: 11,
+                                    goal_kicks: 6,
+                                    offsides: 5,
+                                    fouls: 5,
+                                    ball_possession: 47,
+                                    saves: 4,
+                                    throw_ins: 20,
+                                    shots: 11,
+                                    shots_on_goal: 4,
+                                    shots_off_goal: 7,
+                                    yellow_cards: 3,
+                                    yellow_red_cards: 0,
+                                    red_cards: 0,
+                                },
+                            },
+                            competition: {
+                                id: BigInt(1),
+                                apiId: BigInt(2013),
+                                name: "Campeonato Brasileiro",
+                                areaId: 2032,
+                                code: "BSA",
+                                type: "LEAGUE",
+                                emblem: null,
+                                createdAt: new Date(),
+                                updatedAt: new Date(),
+                            },
+                            elapsed: `${randomNumber(0, 90)}:${randomNumber(0, 59)}`
+                        };
+                    }
+
+                    const matchDetails: MatchType = randomMatch(); // Dados aleatórios completos!
+
+                    // Exemplo: gerar imagem
+                    const imageBuffer = await createMatchImage(matchDetails);
+
+                    const attachment = new AttachmentBuilder(imageBuffer, { name: `${matchDetails.homeTeam.name}-${matchDetails.awayTeam.name}.png` });
+                    await message.reply({ files: [attachment] });
                     break;
                 }
             }
